20e103233a36afde0c05aaf8f0d6a4f5022109ea
Diego Maŕquez <desarrollo.marquez@gmail.com>
2019-06-22 13:10:48 -0500

    Adicion de repositorio de proyectos de java de prueba

---
+++ TestJava/src/com/test/java/Test.java
@@ -0,0 +1,19 @@
+package com.test.java;
+
+public class Test {
+	
+	private Collection<? extends GrantedAuthority> authorities;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("#### Hola Java 8 #### ");
+		System.out.print("\n");
+		
+		
+		
+		
+
+	}
+
+}
